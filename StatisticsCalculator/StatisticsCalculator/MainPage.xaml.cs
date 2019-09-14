384c5d1080618083fab6614c0c9748227f217a37
Fernando Mariano Pereira <fernandovmp@outlook.com>
2019-09-14 18:13:11 -0300

    Tabbed navigation to Main Page

--- StatisticsCalculator/StatisticsCalculator/MainPage.xaml.cs
+++ StatisticsCalculator/StatisticsCalculator/MainPage.xaml.cs
@@ -1,17 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
 
 namespace StatisticsCalculator
 {
-    // Learn more about making custom code visible in the Xamarin.Forms previewer
-    // by visiting https://aka.ms/xamarinforms-previewer
-    [DesignTimeVisible(false)]
-    public partial class MainPage : ContentPage
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainPage : TabbedPage
     {
         public MainPage()
         {
