ddeb94f778863875b31f68107d8cd77e966e3e54
Fernando Mariano Pereira <fernandovmp@outlook.com>
2019-09-14 16:06:26 -0300

    Define ViewModelBase as public

--- StatisticsCalculator/StatisticsCalculator/ViewModels/ViewModelBase.cs
+++ StatisticsCalculator/StatisticsCalculator/ViewModels/ViewModelBase.cs
@@ -6,7 +6,7 @@ using Xamarin.Forms;
 
 namespace StatisticsCalculator.ViewModels
 {
-    class ViewModelBase : BindableObject
+    public class ViewModelBase : BindableObject
     {
         protected void SetProperty<T>(ref T attribute, T value, [CallerMemberName] string propertyName = "")
         {
