036ef597151a36c94d6eae8a7389d06480cd7c19
Fernando Mariano Pereira <fernandovmp@outlook.com>
2019-09-14 16:07:17 -0300

    Added View Model for sample item

---
+++ StatisticsCalculator/StatisticsCalculator/ViewModels/SampleItemViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StatisticsCalculator.ViewModels
+{
+    public class SampleItemViewModel : ViewModelBase
+    {
+        private double _itemValue;
+
+        public SampleItemViewModel(double itemValue)
+        {
+            _itemValue = itemValue;
+        }
+
+        public double ItemValue
+        {
+            get => _itemValue;
+            set => SetProperty(ref _itemValue, value);
+        }
+    }
+}
