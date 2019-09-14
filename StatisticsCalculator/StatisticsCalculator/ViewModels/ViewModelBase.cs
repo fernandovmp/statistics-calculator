2a349ac251865ffbabf2f657b75a921bf3b55301
Fernando Mariano Pereira <fernandovmp@outlook.com>
2019-09-14 14:48:18 -0300

    Added ViewModelBase class

---
+++ StatisticsCalculator/StatisticsCalculator/ViewModels/ViewModelBase.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+using Xamarin.Forms;
+
+namespace StatisticsCalculator.ViewModels
+{
+    class ViewModelBase : BindableObject
+    {
+        protected void SetProperty<T>(ref T attribute, T value, [CallerMemberName] string propertyName = "")
+        {
+            attribute = value;
+            OnPropertyChanged(propertyName);
+        }
+    }
+}
