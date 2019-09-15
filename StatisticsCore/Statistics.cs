923ea2be05fe0f705eaeb6894b4dc081977cfdea
Fernando Mariano Pereira <fernandovmp@outlook.com>
2019-09-15 15:45:51 -0300

    Added statistics core dotnet standard project

---
+++ StatisticsCore/Statistics.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace StatisticsCore
+{
+    public static class Statistics
+    {
+        private static double SumOfItems(int power, params double[] items)
+        {
+            double sum = 0;
+            foreach (var item in items)
+            {
+                sum += Math.Pow(item, power);
+            }
+            return sum;
+        }
+        public static double SumOfItems(params double[] items)
+        {
+            return SumOfItems(1, items);
+        }
+
+        public static double SumOfSquareOfItems(params double[] items)
+        {
+            return SumOfItems(2, items);
+        }
+    }
+}
