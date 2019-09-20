608c2d53f7bf1199d69bdcf8c1452689216a9c91
Fernando Mariano Pereira <fernandovmp@outlook.com>
2019-09-20 17:04:51 -0300

    Added multi language suport

---
+++ StatisticsCalculator/StatisticsCalculator/Translation/StringTranslationExtension.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Reflection;
+using System.Resources;
+using System.Text;
+
+namespace StatisticsCalculator.Translation
+{
+    public static class StringTranslationExtension
+    {
+        public static string Translate(this string text)
+        {
+            if (text == null) return null;
+            Assembly assembly = typeof(StringTranslationExtension).Assembly;
+            AssemblyName assemblyName = assembly.GetName();
+            var resourceManager = new ResourceManager($"{assemblyName.Name}.Resx.AppResources", assembly);
+            string culture = CultureInfo.CurrentCulture.TwoLetterISOLanguageName;
+            return resourceManager.GetString(text, new CultureInfo(culture));
+        }
+    }
+}
---
+++ StatisticsCalculator/StatisticsCalculator/Translation/TranslateExtension.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace StatisticsCalculator.Translation
+{
+    [ContentProperty("Text")]
+    public class TranslateExtension : IMarkupExtension
+    {
+        public string Text { get; set; }
+        public object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return Text == null ? null : Text.Translate();
+        }
+    }
+}
